6c7c571b39601a86046e70eede6346fbf42cb9f1
mini-boo <sgn06221@gmail.com>
2024-05-20 15:38:22 +0900

    프로그래머스 스택/큐 같은 숫자는 싫어

---
+++ JavaAlgorithm/src/HateSameNumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class HateSameNumber {
+    public static int[] solution(int []arr) {
+
+        // stack 또는 queue를 
+        // 어떻게 적용할 수 있을까?
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] != arr[i-1]){
+                queue.add(arr[i-1]);
+            }
+        }
+        queue.add(arr[arr.length - 1]);
+
+        int[] answer = new int[queue.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = queue.poll();
+        }
+
+        return answer;
+    }
+}
